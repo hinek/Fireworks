f381fbf46b14e0e02404f1cbe93e4551cb43fbd6
hinek <hinek@gmx.de>
2019-12-31 08:46:01 +0100

    0.2 (2019-12-28)
    - min and max velocity
    - friction
    - resized particles to 2x2 pixels
    - fix position of objects
    - effects with duration
    - three firework types (simple rocket, fountain, sparkle rocket)

--- changeLog.md
+++ changeLog.md
@@ -5,3 +5,10 @@
 - basic model for rockets and projectiles
 - gravity
 - min and max angle, single velocity
+
+## 0.2 (2019-12-28)
+- min and max velocity
+- friction
+- fix position of objects
+- effects with duration
+- three firework types (simple rocket, fountain, sparkle rocket)
--- fireworks.js
+++ fireworks.js
@@ -12,11 +12,12 @@ var addFirework = function(firework, startTime, x) {
 var createProjectiles = function(x, y, instructions) {
   for(var i = 0; i < instructions.count; i++) {
     angle = Math.random() * (instructions.maxAngle - instructions.minAngle) + instructions.minAngle;
+    velocity = Math.random() * (instructions.maxVelocity - instructions.minVelocity) + instructions.minVelocity;
     projectiles.push({
       x: x,
       y: y,
-      vx: instructions.velocity * Math.sin(angle),
-      vy: instructions.velocity * Math.cos(angle) * -1
+      vx: velocity * Math.sin(angle),
+      vy: velocity * Math.cos(angle) * -1
     });
   }
 }
@@ -43,7 +44,8 @@ var iterate = function(elapsed) {
         vx: element.firework.vx,
         vy: element.firework.vy,
         timeToNextStep: element.firework.timeToNextStep,
-        nextStep: Object.assign({}, element.firework.nextStep) // clone object
+        nextStep: Object.assign({}, element.firework.nextStep), // clone object
+        positionFixed: element.firework.positionFixed
       });
       // delete element
       fireworks.splice(i, 1);
@@ -54,6 +56,9 @@ var iterate = function(elapsed) {
   while (i--) {
     element = projectiles[i];
 
+    element.vx *= 0.98;
+    element.vy *= 0.98;
+
     element.vy += g * (elapsed / 1000);
 
     if (element.nextStep) {
@@ -61,14 +66,22 @@ var iterate = function(elapsed) {
       if (element.timeToNextStep <= 0) {
         // create new projectiles
         createProjectiles(element.x, element.y, element.nextStep);
-        // delete projectile
-        projectiles.splice(i, 1);
-        continue;
+        if (element.nextStep.duration && element.nextStep.duration > 0) {
+          element.nextStep.duration -= elapsed;
+        }
+        else {
+          element.nextStep = null;
+          // delete projectile
+          projectiles.splice(i, 1);
+          continue;
+        }
       }
     }
 
-    element.x += element.vx * (elapsed / 10);
-    element.y += element.vy * (elapsed / 10);
+    if (!element.positionFixed) {
+      element.x += element.vx * (elapsed / 10);
+      element.y += element.vy * (elapsed / 10);
+    }
 
     if (element.x < 0 || element.x > canvas.width || element.y < 0 || element.y > canvas.height) {
       projectiles.splice(i, 1);
